c477f24df172c22eed7bfd5cbfb2358ddbec4c84
sev <sev@ap-soft.ru>
2025-10-08 23:50:45 +0500

    initialize project

---
+++ src/main/java/ru/anykeyers/productionplannerstorage/ProductionPlannerStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.anykeyers.productionplannerstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionPlannerStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionPlannerStorageApplication.class, args);
+    }
+
+}
